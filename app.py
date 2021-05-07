781a4fae41f3c332e0cf68169c99447f82e1ec35
tomten <tomten@pop-os.localdomain>
2021-05-07 19:22:39 +0200

    Deletes removed files from branch and adds unit test for subtraction

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from handlers.calculator_api import configure_api
-
-app = Flask(__name__)
-
-configure_api(app)
-
-if __name__ == '__main__':
-    app.run()
--- test_calculator.py
+++ test_calculator.py
@@ -3,3 +3,7 @@ import calculator as calc
 
 def test_add():
     assert calc.add(5, 10) == 15
+
+
+def test_sub():
+    assert calc.sub(10, 5) == 5
