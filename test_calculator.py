cc03518ba0514bc955ba1836f4d6e414db0856da
tomten <tomten@pop-os.localdomain>
2021-05-07 19:25:29 +0200

    Adds more asserts to test and implements subtraction in calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -3,7 +3,9 @@ import calculator as calc
 
 def test_add():
     assert calc.add(5, 10) == 15
+    assert calc.add(15, 10) == 25
 
 
 def test_sub():
     assert calc.sub(10, 5) == 5
+    assert calc.sub(25, 5) == 20
